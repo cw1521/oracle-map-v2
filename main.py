f6fc5c6c1fceec165a1d197b2eb0bda5fd6165f6
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-04 19:15:33 -0500

    finished ner tags

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ def get_input_sentence(obj):
             output += f"{key} {' '.join(str(elem) for elem in obj[key])}"
         else:
             output += f'{key} {str(obj[key])}'
-
     return output
 
 
@@ -60,6 +59,8 @@ def get_direction(heading):
 
 
 
+
+
 # temporary work around input file format
 def get_action_obj(action):
     action_obj = {}
@@ -73,8 +74,29 @@ def get_action_obj(action):
 
 
 
+def get_pos_ner_tags(pos_sentence):
+    tokens = pos_sentence.strip().split()
+    pos_labels = [
+        "quadrant", "1", "2", "3", "4",
+        "orange", "blue", "goal", "center",
+        "wall", "north", "south", "east", "west"
+    ]
+
+    ner_tags = []
+    for token in tokens:
+        if token in pos_labels:
+            ner_tags.append(16)
+        else:
+            ner_tags.append(0)
+
+    return ner_tags
+
+
+
+
 def get_position_sentence(x,y):
     pos = []
+
     if x >= 0 and y >= 0:
         pos.append('in quadrant 1')
     if x < 0 and y >= 0:
@@ -97,8 +119,11 @@ def get_position_sentence(x,y):
         pos.append('near the south wall')
     if y >= 4120:
         pos.append('near the north wall')
+
     sentence = f"I'm {pos[0]} {' and '.join(pos[1:])}".strip() +'.'
-    return sentence
+    ner_tags = get_pos_ner_tags(sentence)
+
+    return sentence, ner_tags
 
 
 
@@ -149,16 +174,19 @@ def replace_placeholder(text, value):
 def get_sentence(obj, percept=None, action=None):
     template = get_sentences_template()
     index = get_index()
+    ner_tags = []
     if percept:
         sentence = template[percept][index][0]
+        ner_tags = template[percept][index][1]
         if percept != "direction":
             value = obj[percept]
         else:
             value = get_direction(obj[percept])
     else:
         sentence = template["action"][action][index][0]
-        value = obj[action]
-    return replace_placeholder(sentence, value), sentence[1]
+        ner_tags = template["action"][action][index][1]
+        value = get_action(action, obj[action])
+    return replace_placeholder(sentence, value), ner_tags
 
 
 
@@ -170,11 +198,12 @@ def get_target_sentence(obj, action):
     pos = obj['position']
     sentence = ''
     template = get_sentences_template()
+    ner_tags = []
 
     # select sentences from the template and replace the attributes as necessary
     if obj['is_demoed']:
         index = get_index()
-        return template['is_demoed'][index][0]
+        return template['is_demoed'][index][0], template['is_demoed'][index][1]
 
     # Create sentences for each percept and action
     num_sentences = 7
@@ -188,7 +217,8 @@ def get_target_sentence(obj, action):
             case 0:
                 if obj['boost_amount'] > 0:
                     text = get_sentence(obj, "boost_amount")
-                    sentence += f" {text}"
+                    sentence += f" {text[0]}"
+                    ner_tags += text[1]
 
             # Percept(on_ground)
             case 1:
@@ -196,42 +226,53 @@ def get_target_sentence(obj, action):
                 if obj['on_ground']:
                     index = get_index()
                     sentence += f" {template['on_ground'][index][0]}"
-
+                    ner_tags += template['on_ground'][index][1]
             # Percept(speed)
             case 2:
                 text = get_sentence(obj, "speed")
-                sentence += f" {text}"
+                sentence += f" {text[0]}"
+                ner_tags += text[1]
 
             # Percept(Direction)
             case 3:
                 text = get_sentence(obj, "direction")
-                sentence += f" {text}"
+                sentence += f" {text[0]}"
+                ner_tags += text[1]
 
             # Percept(Position)
             case 4:
-                sentence += f" {get_position_sentence(pos[0], pos[1])}"
+                text = get_position_sentence(pos[0], pos[1])
+                sentence += f" {text[0]}"
+                ner_tags += text[1]
 
             # Action(Brake) or (Action(Steer) and/or Action(Throttle))
             case 5:
                 if action['handbrake']:
                     index = get_index()
-                    sentence += f" {template['action']['handbrake'][index]}"
+                    sentence += f" {template['action']['handbrake'][index][0]}"
+                    ner_tags += template['action']['handbrake'][index][1]
                 else:
-                    text1 = text = get_sentence(action, action="steer")
-                    text2 = text = get_sentence(action, action="throttle")
+                    text1 = get_sentence(action, action="steer")
+                    text2 = get_sentence(action, action="throttle")
 
-                    temp1 = f" {text1}".replace('.', '')
-                    temp2 = f"{text2}"
+                    temp1 = f" {text1[0]}".replace('.', '')
+                    temp2 = f"{text2[0]}"
                     sentence += f" {' and '.join([temp1, temp2])}"
+
+                    ner_tags += text1[1]
+                    ner_tags += text2[1]
 
             # Action(Boost)
             case 6:
                 if action['boost']:
                     index = get_index()
                     sentence += f" {template['action']['boost'][index][0]}"
+                    ner_tags += template['action']['boost'][index][1]
 
-
-    return sentence.strip()
+    # ner_tags.append(0)
+    sentence = sentence.replace("  ", " ").strip()
+    print(sentence.split(' '), len(ner_tags), len(sentence.split(' ')))
+    return sentence, ner_tags
 
 
 
@@ -289,66 +330,66 @@ def get_ner_tag_map():
 def get_sentences_template():
     template = {
         'is_demoed': [
-            ['My car has been demolished.', [0,0,1,0,1]],
-            ['My car has exploded!', [0,0,1,1]],
-            ['I crashed my car!', [0,1,0,0]],
-            ['I wrecked my car.', [0,1,0,0]]
+            ['My car has been demolished.', [0,0,1,0,1,0]],
+            ['My car has exploded!', [0,0,1,1,0]],
+            ['I crashed my car!', [0,1,0,0,0]],
+            ['I wrecked my car.', [0,1,0,0,0]]
         ],
         'boost_amount': [
-            ['My current boost is *r.', [0,2,2,0,3]],
-            ['I currenly have *r percent boost.', [0,2,0,3,0,2]],
-            ['I have *r boost.', [0,0,3,2]],
-            ['My boost is *r percent.', [0,2,0,3,0]],
+            ['My current boost is *r.', [0,2,2,0,3,0]],
+            ['I currenly have *r percent boost.', [0,2,0,3,0,2,0]],
+            ['I have *r boost.', [0,0,3,2,0]],
+            ['My boost is *r percent.', [0,2,0,3,0,0]],
         ],
         'on_ground': [
-            ['My car is on the ground.', [0,0,0,4,0,4]],
-            ["I'm on the ground.", [0,4,0,4]],
-            ["I'm not in the air.", [0,4,0,0,4]],
-            ["I'm currently driving on the ground.", [0,0,0,4,0,4]]
+            ['My car is on the ground.', [0,0,0,4,0,4,0]],
+            ["I'm on the ground.", [0,4,0,4,0]],
+            ["I'm not in the air.", [0,4,0,0,4,0]],
+            ["I'm currently driving on the ground.", [0,0,0,4,0,4,0]]
         ],
         'ball_touched': [
-            ['I have the ball!', [0,5,0,5]],
-            ["I've got the ball!", [0,5,0,5]],
-            ["I currently have the ball.", [0,0,5,0,5]],
-            ["The ball is in my possession.", [0,5,0,0,5,5]],
+            ['I have the ball!', [0,5,0,5,0]],
+            ["I've got the ball!", [0,5,0,5,0]],
+            ["I currently have the ball.", [0,0,5,0,5,0]],
+            ["The ball is in my possession.", [0,5,0,0,5,5,0]],
         ],
         'speed': [
-            ['My current speed is *r.', [0,0,6,0,7]],
-            ["I'm travelling *r miles per hour.", [0,0,7,6,6,6]],
-            ['My current speed is *r mph.', [0,0,6,0,7,6]],
-            ['My current speed is *r miles per hour.', [0,0,6,0,7,6,6,6]]
+            ['My current speed is *r.', [0,0,6,0,7,0]],
+            ["I'm travelling *r miles per hour.", [0,0,7,6,6,6,0]],
+            ['My current speed is *r mph.', [0,0,6,0,7,6,0]],
+            ['My current speed is *r miles per hour.', [0,0,6,0,7,6,6,6,0]]
         ],
         'direction': [
-            ["I'm currently travelling *r.", [0,8,8,9]],
-            ["I'm heading in the *r direction.", [0,8,0,0,9,8]],
-            ['My current direction is *r.', [0,8,8,0,9]],
-            ["I'm heading *r.", [0,8,9]],
+            ["I'm currently travelling *r.", [0,8,8,9,0]],
+            ["I'm heading in the *r direction.", [0,8,0,0,9,8,0]],
+            ['My current direction is *r.', [0,8,8,0,9,0]],
+            ["I'm heading *r.", [0,8,9,0]],
         ],
         'position': [],
         'action': {
             'handbrake': [
-                ["I'm currently braking.", [0,10,10]],
-                ["I pressed the brakes.", [0,10,0,10]],
-                ["I'm stopping", [0,10]],
-                ["I stopped.", [0,10]],
+                ["I'm currently braking.", [0,10,10,0]],
+                ["I pressed the brakes.", [0,10,0,10,0]],
+                ["I'm stopping", [0,10,0]],
+                ["I stopped.", [0,10,0]],
             ],
             'steer': [
-                ["I'm steering *r.", [0,11,12]],
-                ["I'm turning *r.", [0,11,12]],
-                ['I turned *r.', [0,11,12]],
-                ["I'm about to turn *r.", [0,0,0,11,12]],
+                ["I'm steering *r.", [0,11,12,0]],
+                ["I'm turning *r.", [0,11,12,0]],
+                ['I turned *r.', [0,11,12,0]],
+                ["I'm about to turn *r.", [0,0,0,11,12,0]],
             ],
             'throttle': [
-                ["I'm driving *r.", [0,13,14]],
-                ["I'm going *r.", [0,13,14]],
-                ["I'm moving *r.", [0,13,14]],
-                ["I'm travelling *r.", [0,13,14]]
+                ["I'm driving *r.", [0,13,14,0]],
+                ["I'm going *r.", [0,13,14,0]],
+                ["I'm moving *r.", [0,13,14,0]],
+                ["I'm travelling *r.", [0,13,14,0]]
             ],
             'boost': [
-                ["I've used boost.", [0,15,15]],
-                ["I'm using boost.", [0,15,15]],
-                ["I've used the speed up.", [0,15,0,15,15]],
-                ["I have boosted.", [0,15,15]]
+                ["I've used boost.", [0,15,15,0]],
+                ["I'm using boost.", [0,15,15,0]],
+                ["I've used the speed up.", [0,15,0,15,15,0]],
+                ["I have boosted.", [0,15,15,0]]
             ]
         }        
     }
@@ -359,23 +400,9 @@ def get_sentences_template():
 
 
 
-
-
-def get_relation_sentence(data, action):
-    print(f'Data: {data}\nAction: {action}')
-
-
-
-
-
-
-
 def remove_duplicates(dataset):
     set_of_jsons = set([dumps(d, sort_keys=True) for d in dataset])
     return [loads(t) for t in set_of_jsons]
-
-
-
 
 
 
@@ -394,23 +421,13 @@ def get_oracle(dataset):
             data = data['state']['measurements']
             action_string = ' '.join(f'{key} {action[key]}' for key in action.keys())
             obj['input'] = f"{get_input_sentence(data)} {action_string}".replace('  ', ' ')
-            obj['target'] = get_target_sentence(data, action).replace('  ', ' ')
-
-            obj['relation'] = get_relation_sentence(data, action)
-
-
-
-
-
-            # data_list.append(obj)
-
-
-
-
-    # print(len(data_list))
-    # oracle['all_data'] = remove_duplicates(data_list)
-    # print(len(oracle['all_data']))
-    # return split_dataset(oracle['all_data'])
+            obj['target'], obj['ner_tags'] = get_target_sentence(data, action)
+            obj['target'] = obj['target'].replace('  ', ' ')
+            data_list.append(obj)
+    print(len(data_list))
+    oracle['all_data'] = remove_duplicates(data_list)
+    print(len(oracle['all_data']))
+    return split_dataset(oracle['all_data'])
     
 
 
@@ -447,6 +464,7 @@ def write_train_oracle(oracle):
             dump({'data':oracle['train'][i*seg:(i+1)*seg]}, f, indent=2)   
 
 
+
 def write_oracle(oracle):
     write_train_oracle(oracle)
     with open(TEST_OUTPUT_PATH, 'w') as f:
@@ -459,13 +477,9 @@ def write_oracle(oracle):
 def main():
     seed(10)
     dataset = get_dataset(INPUT_PATH)
-
-    test_dataset = dataset[0:100]
-
-
+    test_dataset = dataset
     oracle = get_oracle(test_dataset)
-    # print(oracle)
-    # write_oracle(oracle)
+    write_oracle(oracle)
 
 
 
